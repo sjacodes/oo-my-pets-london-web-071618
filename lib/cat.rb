1b208b2f4067b6711d5d6bc7c9546c22258fab21
Sarah Jacob <smjacob91@gmail.com>
2018-07-19 14:48:48 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,15 @@
 class Cat
-  # code goes here
+  
+  attr_reader :name
+  attr_accessor :mood, :owner
+   
+   def initialize(name)
+     @name = name
+     @mood = "nervous"
+   end
+  
 end
+
+
+
+
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,26 @@
 class Owner
-  # code goes here
+  
+  attr_accessor :pets, :name
+  
+  OWNERS = []
+  
+  
+  def initialize(pets)
+    @name = name
+    @pets = {fishes: [], cats: [], dogs: []}
+    OWNERS << self
+  end
+  
+  def self.reset_all
+    OWNERS.clear
+  end
+  
+  def self.all
+    OWNERS
+  end
+  
+  def self.count
+    OWNERS.length
+  end
+  
 end
