d6c7f4e4f0cd36f529caa5b300c9f5bbffb32cc6
Sarah Jacob <smjacob91@gmail.com>
2018-07-19 15:19:56 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,12 +1,14 @@
 class Owner
   
+  attr_reader :species
   attr_accessor :pets, :name
+  
   
   OWNERS = []
   
   
-  def initialize(pets)
-    @name = name
+  def initialize(species)
+    @species = species
     @pets = {fishes: [], cats: [], dogs: []}
     OWNERS << self
   end
@@ -22,5 +24,14 @@ class Owner
   def self.count
     OWNERS.length
   end
+  
+  def say_species
+    return "I am a #{species}."
+  end
+  
+  def buy_fish
+    @pets[:fishes] << Fish.new(name)
+  end
+    
   
 end
